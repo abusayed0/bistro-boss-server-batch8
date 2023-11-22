80244ec3320ce3acc44f97df720ab904c749442e
abusayed0 <abusayed48876@gmail.com>
2023-11-22 23:13:49 +0600

    created get api for menu, review and get, post, delete api for carts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 
@@ -31,16 +31,39 @@ async function run() {
         const bistroBossDB = client.db("bistroBossDB");
         const menuCollection = bistroBossDB.collection("menuCollection");
         const reviewCollection = bistroBossDB.collection("reviewCollection");
+        const cartCollection = bistroBossDB.collection("cartCollection");
 
-        app.get("/menu", async(req, res) => {
+        app.get("/menu", async (req, res) => {
             const query = {};
             const cursor = await menuCollection.find(query).toArray();
             res.send(cursor)
         });
-        app.get("/reviews", async(req, res) => {
+        app.get("/reviews", async (req, res) => {
             const query = {};
             const cursor = await reviewCollection.find(query).toArray();
             res.send(cursor)
+        });
+
+        // cart collection 
+        app.get("/carts", async (req, res) => {
+            const email = req.query.email;
+            const query = {email};
+            const cursor = await cartCollection.find(query).toArray();
+            res.send(cursor);
+        });
+
+        app.post("/carts", async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result);
+        });
+
+        app.delete("/carts/:id", async(req, res) => {
+            const id = req.params.id;
+           
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
         });
 
 
